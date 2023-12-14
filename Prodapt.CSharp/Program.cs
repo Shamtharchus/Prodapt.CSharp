6b5fe39d8f26ea3d5d5ab08947e6f2413c1c63b9
sham.t <sham.t@CLAP1285>
2023-12-14 11:00:24 +0530

    Add project files.

---
+++ Prodapt.CSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Prodapt.CSharp.Library;
+
+namespace Prodapt.Csharp.Day4
+{
+    internal class Example1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome");
+            Login obj = new Login();
+            var result = obj.Validation("admin", "admin@123");
+            Console.WriteLine(result ? "Valid User" : "Invalid User");
+        }
+    }
+}
---
+++ Prodapt.Github/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Prodapt.Github
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var today = DateTime.Now;
+            Console.WriteLine($"Today:{today}");
+            Console.WriteLine($"Date Only:{today.ToShortDateString()}");
+            Console.WriteLine($"Date Only:{DateTime.Now.ToShortDateString()}");
+            Console.WriteLine($"Time Only:{today.ToShortTimeString()}");
+            Console.WriteLine($"Time Only:{DateTime.Now.ToShortTimeString()}");
+            Console.WriteLine($"Long Date:{today.ToLongDateString()}");
+            Console.WriteLine($"LongTimeOnly:{today.ToLongTimeString()}");
+
+        }
+    }
+}
